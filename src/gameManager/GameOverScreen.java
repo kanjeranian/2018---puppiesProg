5297ae89cd5154e42b95d623cea08ce19b5aab88
kanjeranian <oilscius@gmail.com>
2019-05-10 16:45:49 +0700

    add GameOverScreen but can delete

---
+++ src/gameManager/GameOverScreen.java
@@ -0,0 +1,55 @@
+package gameManager;
+
+import java.util.ArrayList;
+
+import SharedObject.AllObj;
+import character.Ghost;
+import character.Puppy;
+import character.Puppy1;
+import character.Puppy2;
+import constant.Img;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import logic.Point;
+import others.Block;
+import logic.Hitbox;
+
+public class GameOverScreen extends StackPane {
+	
+	private static final double W = 1200, H = 900;
+    private Scene scene;
+    private GraphicsContext gc;
+    private KeyPress keyPress;
+
+    public GameOverScreen() {    	
+    	Canvas cv = new Canvas(W, H);
+    	gc = cv.getGraphicsContext2D();
+    	Pane root = new Pane(cv);
+    	scene = new Scene(root);
+    	keyPress = new KeyPress(scene);
+	}
+	
+	public Scene getGameScene() {
+		return scene;
+	}
+	
+	public KeyPress getKeyPress() {
+		return keyPress;
+	}
+	
+	public GraphicsContext getGc() {
+		return gc;
+	}
+
+}
